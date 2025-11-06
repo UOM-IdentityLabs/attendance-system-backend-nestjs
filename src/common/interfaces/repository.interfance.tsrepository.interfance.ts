ebac5dcdb42d36b1a1722d6ce4e8a58ba7702db4
AbdulahKG <abdullah.aboode.omar@gmail.com>
2025-11-06 20:56:11 +0300

    feat: add JWT and mapped types support, and bcrypt dependency

---
+++ src/common/interfaces/repository.interfance.tsrepository.interfance.ts
@@ -0,0 +1,9 @@
+import { DeleteResult } from 'typeorm';
+
+export interface IRepository<T, D, B, C> {
+  create(createDto: D, user?: any): Promise<T>;
+  getAll(query: B);
+  getById(id: number | string, query: B): Promise<T>;
+  update(id: number | string, updateDto: C): Promise<T>;
+  delete(id: number | string): Promise<DeleteResult>;
+}
