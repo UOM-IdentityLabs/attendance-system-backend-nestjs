5b5f0fa53717c4f84ddaaad458fdb6bc5a63aa81
AbdulahKG <abdullah.aboode.omar@gmail.com>
2026-02-19 02:39:38 +0300

    feat: update create method to return a single or array of entities

--- src/common/interfaces/repository.interfance.tsrepository.interfance.ts
+++ src/common/interfaces/repository.interfance.tsrepository.interfance.ts
@@ -1,7 +1,7 @@
 import { DeleteResult } from 'typeorm';
 
 export interface IRepository<T, D, B, C> {
-  create(createDto: D, req?: any): Promise<T>;
+  create(createDto: D, req?: any): Promise<T | T[]>;
   getAll(query: B, req?: any);
   getById(id: number | string, query: B): Promise<T>;
   update(id: number | string, updateDto: C): Promise<T>;
